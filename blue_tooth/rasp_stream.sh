4ebc91cd5d54bae296abecce707cc1846e4f0a1a
sagitaurusram <sagitaurus.ram@gmail.com>
2020-08-11 22:40:11 +0530

    updated buttons and video

--- blue_tooth/rasp_stream.sh
+++ blue_tooth/rasp_stream.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/pi/bot_tmp/mjpg-streamer/mjpg-streamer-experimental
-./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so"
+./mjpg_streamer -o "output_http.so -w ./www" -i "input_raspicam.so -rot 180"
